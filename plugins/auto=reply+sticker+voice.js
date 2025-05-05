5b5168dfb6cb54595caafa521fcf99307320c568
RAVINDU_MD <Katharinabto@gmail.com>
2025-05-05 13:53:49 +0530

    Add files via upload

---
+++ plugins/auto=reply+sticker+voice.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const config = require("../config");
+const path = require('path');
+
+const {cmd , commands} = require('../command')
+
+//auto_voice
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../data/autovoice.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_VOICE === 'true') {
+                //if (isOwner) return;        
+                await conn.sendPresenceUpdate('recording', from);
+                await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+            }
+        }
+    }                
+});
